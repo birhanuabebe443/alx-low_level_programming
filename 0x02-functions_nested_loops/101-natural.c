450bc0a6e5c4a31a64f13b8ba0fb75bc60d2e477
abebe wale <abebebirhanu2007@example.com>
2022-07-07 09:16:54 -0700

    C - Functions, nested loops commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * * main - Prints the sum of all multiples of 3 or 5 up to 1024
- * *
- * * Return: Always (Success)
- * */
+ * main - Prints the sum of all multiples of 3 or 5 up to 1024
+ * Return: Always (Success)
+ */
 int main(void)
 {
 	int i, z = 0;
