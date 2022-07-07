76fe51743c8fe40ae0ffab1a2033ccc1f04cacf1
abebe wale <abebebirhanu2007@example.com>
2022-07-07 07:41:57 -0700

    C - Functions, nested loops commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -14,9 +14,8 @@ int main(void)
 						_putchar('c');
 							_putchar('h');
 								_putchar('a');
-									_putchar('r');
-										_putchar('t');
-											_putchar('\n');
+								_putchar('r');
+								_putchar('\n');
 
 												return (0);
 }
