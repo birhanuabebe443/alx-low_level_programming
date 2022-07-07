3c62696c9976e26140e9169ec194ecd599173cea
abebe wale <abebebirhanu2007@example.com>
2022-07-07 09:29:02 -0700

    C - Functions, nested loops commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,7 +2,7 @@
 #define LARGEST 10000000000
 /**
  *  main - main block
- *   Description: Find and print the first 98 fib numbers starting with 1 and 2.
+ *   Description: Find and print the first 98 fib  with 1 and 2.
  *    Numbers should be coma and space separated.
  *     Return: 0
  */
