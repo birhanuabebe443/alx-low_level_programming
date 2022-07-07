17ff052368842d39298189b9d3d20b9e5c9b0b3a
abebe wale <abebebirhanu2007@example.com>
2022-07-07 08:09:03 -0700

    C - Functions, nested loops commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,7 @@
 
 /**
  *    print_alphabet_x10 - Make alphabet x10 times
- *      
- *         Return: void
+ *    Return: void
  */
 void print_alphabet_x10(void)
 {
@@ -16,7 +15,7 @@ void print_alphabet_x10(void)
 													{
 														_putchar(c);
 													}
-		_putchar('\n');
-												i++;
-		}
+			_putchar('\n');
+	 												i++;
+	  	}
 }
