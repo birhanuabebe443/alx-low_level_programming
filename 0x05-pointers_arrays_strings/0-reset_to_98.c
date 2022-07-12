2e07c08815671b1e6b2f0a34b4a120519fb08f25
abebe wale <abebebirhanu2007@example.com>
2022-07-11 20:44:46 -0700

    C - Pointers, arrays and strings project commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *reset_to_98 - function with one argument
- *@n: int type pointer n
- * Description: using pointer as parameter to update value 
- * Return: value
+ *  reset_to_98 - function with one argument
+ *   @n: int type pointer n
+ *    Description: using pointer as parameter to update value to 98
+ *     Return: value
  */
 void reset_to_98(int *n)
 {
